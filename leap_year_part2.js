b8207ade42cee496aaaee5b6c0e8b1e2e982f306
Birhanu Kassa <ricardogebremichael@gmail.com>
2020-12-14 15:31:58 -0600

    Leap Years (Part 2)

---
+++ leap_year_part2.js
@@ -0,0 +1,25 @@
+//Leap Years (Part 2)
+
+function isLeapYear(year) {
+  let GregorianCalendarDay = 1752;
+  if(year > GregorianCalendarDay) {
+    return (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0); 
+  } else {
+    return (year < GregorianCalendarDay && year > 3 && year % 4 == 0);  
+  }
+}
+
+
+console.log(isLeapYear(2016));      // true
+console.log(isLeapYear(2015));      // false
+console.log(isLeapYear(2100));      // false
+console.log(isLeapYear(2400));      // true
+console.log(isLeapYear(240000));    // true
+console.log(isLeapYear(240001));    // false
+console.log(isLeapYear(2000));      // true
+console.log(isLeapYear(1900));      // false
+console.log(isLeapYear(1752));      // true
+console.log(isLeapYear(1700));      // true
+console.log(isLeapYear(1));         // false
+console.log(isLeapYear(100));       // true
+console.log(isLeapYear(400));       // true
