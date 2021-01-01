fb10618e1dfd09bcb40bfa84835122f27e7581f9
Birhanu Kassa <ricardogebremichael@gmail.com>
2020-12-31 19:46:21 -0600

    Exclusive Or

---
+++ exclusive_or.js
@@ -0,0 +1,18 @@
+
+// const xor = function (param1, param2) {
+//     if(param1 )
+//     return param1 || param2;
+// }
+//const xor = (param1, param2) => param2 || param1;
+
+const xor = function (param1, param2) {
+    return !!((param1 && !param2) || (!param1 && param2));       
+}
+
+
+
+
+console.log(xor(5, 0) === true);
+console.log(xor(false, true) === true);
+console.log(xor(1, 1) === false);
+console.log(xor(true, true) === false);
