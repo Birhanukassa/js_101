0552ac18746404f971b46650c9b71b4fe710f89a
Birhanu <ricardogebremichael@gmail.com>
2021-03-10 18:05:05 -0600

    Add files via upload

---
+++ lesson_3/exclamationMark.js
@@ -0,0 +1,21 @@
+/**
+Question 2
+How can you determine whether a given string ends
+with an exclamation mark (!)?
+
+*/                                  
+
+let str1 = "Come over here!"; // true
+let str2 = "What's up, Doc?"; // false
+
+
+function lastChar(param) {
+   return param = param.slice(-1) === '!';  
+}
+
+// str1.endsWith("!"); // true
+// str2.endsWith("!"); // false
+
+ console.log(lastChar(str1)); // true
+ console.log(lastChar(str2)); // false
+
