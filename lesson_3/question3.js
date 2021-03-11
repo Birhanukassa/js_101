eebaca6959937de34da7f07d06e26f7fd8b63d1e
Birhanu <ricardogebremichael@gmail.com>
2021-03-10 18:52:09 -0600

    Add files via upload

---
+++ lesson_3/question3.js
@@ -0,0 +1,5 @@
+// Determine whether the following object of people and their age contains an entry for 'Spot':
+
+let ages = { Herman: 32, Lily: 30, Grandpa: 402, Eddie: 10 };
+
+console.log(ages.hasOwnProperty('spot'));
