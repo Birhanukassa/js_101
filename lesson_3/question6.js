a4ece54ab0fa27cfa4480acb3cb7ddb97dc30782
Birhanu <ricardogebremichael@gmail.com>
2021-03-11 21:21:25 -0600

    Oject.assign in action

---
+++ lesson_3/question6.js
@@ -0,0 +1,8 @@
+// We have most of the Munster family in our ages object:
+// Add entries for Marilyn and Spot to the object:
+
+let ages = { Herman: 32, Lily: 30, Grandpa: 5843, Eddie: 10 };
+
+let additionalAges = { Marilyn: 22, Spot: 237 };
+
+console.log(Object.assign(ages, additionalAges));
