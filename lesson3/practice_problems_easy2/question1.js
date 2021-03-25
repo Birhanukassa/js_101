75e878e7d2459ec43e343e780eb2366413e2f9a2
Birhanu <ricardogebremichael@gmail.com>
2021-03-25 02:45:28 -0500

    Create question1.js

---
+++ lesson3/practice_problems_easy2/question1.js
@@ -0,0 +1,13 @@
+/*
+Practice Problems: Easy 2
+
+Question 1
+Given a string, return a new string that replaces every occurrence
+of the word "important" with "urgent":
+*/
+
+let advice = "important Few things in life are as important as house training your pet important dinosaur.";
+
+console.log(advice.replace("important", "urgent"));
+let replace = advice.replace(/important/g, 'ugrent');
+console.log(replace);
