5947698c575078b3739da48e038f34380ab2ba5d
Birhanu <ricardogebremichael@gmail.com>
2021-03-25 02:52:54 -0500

    Delete lesson_1 directory

--- lesson_1/snippet_prompt.js
+++
@@ -1,5 +0,0 @@
-function getName(prompt) {
-    let readlineSync = require('readline-sync');
-    let name = readlineSync.question(prompt);
-    return name;
-  }
--- lesson_1/the_end_is_near_but_not_here.js
+++
@@ -1,9 +0,0 @@
-
-function penultimate(str) {
-  let splitstr = str.split(' ');
-  return splitstr[splitstr.length - 2];
-};
-
-
-console.log(penultimate("last word") === "last"); // logs true
-console.log(penultimate("Launch School is great!") === "is"); // logs true
