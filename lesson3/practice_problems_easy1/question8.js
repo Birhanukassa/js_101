f3538335070f4889c04d8069e370441fbc74cc3b
Birhanu <ricardogebremichael@gmail.com>
2021-03-25 23:21:43 -0500

    Delete lesson3 directory

--- lesson3/practice_problems_easy1/question8.js
+++
@@ -1,7 +0,0 @@
-// How can we add the family pet, "Dino", to the following array?
-
-let flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"];
-
-flintstones.push('DIno');
-console.log(flintstones);
-
