058543b4ca0ce50087b6665d1d4b26a11111c12e
Birhanu <ricardogebremichael@gmail.com>
2021-03-25 23:32:34 -0500

    Add files via upload

---
+++ lesson_3/practice_problems_easy1/question5.js
@@ -0,0 +1,10 @@
+/*
+Easy 1
+* Question 5 //
+What will the following code output?
+console.log(false == '0');
+console.log(false === '0');
+true
+false
+
+*/
---
+++ lesson_3/practice_problems_easy1/question8.js
@@ -0,0 +1,7 @@
+// How can we add the family pet, "Dino", to the following array?
+
+let flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"];
+
+flintstones.push('DIno');
+console.log(flintstones);
+
